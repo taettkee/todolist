3aca4c1b27aed635c8f0250a17fc06bcc99aaf4f
taettkee <48561520+taettkee@users.noreply.github.com>
2019-11-25 22:43:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-async function fetchData (url, info) {
-    const options = {
-        method:'POST',
-        headers: {
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify(info)
+$(async function(){
+    async function fetchData (url, info) {
+        const options = {
+            method:'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(info)
+        }
+        let response = await fetch(url,options)
+        let data = await response.json()
+        return data
     }
-    let response = await fetch(url,options)
-    let data = await response.json()
-    return data
-}
-let data = await fetchData('https://taettkee.github.io/todolist/', {'giveme' : ''})
-console.log(data)
+    let data = await fetchData('https://taettkee.github.io/todolist/', {'giveme' : ''})
+    console.log(data)
+})
